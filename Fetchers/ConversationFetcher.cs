b260e0aba9f2240bcf58884f4a29e5da0cf9b0e0
myGithub123# <royandugu99009@gmail.com>
2022-10-17 19:48:06 +0545

    New format

--- Fetchers/ConversationFetcher.cs
+++ Fetchers/ConversationFetcher.cs
@@ -3,15 +3,9 @@ using System.IO;
 using UnityEngine;
 public class ConversationFetcher:MonoBehaviour
 {
+
     public void GetConversation(NPC n){
-        string jsonFile=File.ReadAllText("Assets/Scripts/Statics/CharacterConversationScripts/"+n.GetName()+".json");
-        var compInstance = (Component)Activator.CreateInstance(n.GetType());
-        // compInstance=JsonUtility.FromJson(jsonFile,compInstance.GetType()) ??; 
     }
     private void Start() {
-        Sharon s;
-        GameObject go=new GameObject("Sharon");
-        s=go.AddComponent<Sharon>();
-        GetConversation(s);
     }
 }
--- Statics/NPCs/Npc.cs
+++ Statics/NPCs/Npc.cs
@@ -1,4 +1,14 @@
-public interface NPC{
-    string GetName(); 
-    string GetText(int phaseValue, int index);
+using System.IO;
+using UnityEngine;
+public class NPC:MonoBehaviour{
+    [System.Serializable]
+    public class Sharon{
+        public string[] phaseOne;
+        public string[] phaseTwo;
+    }
+    private void Start() {
+        string jsonString=File.ReadAllText("Assets/Scripts/Statics/CharacterConversationScripts/characterDialogues.json");
+        Sharon sharonDialogs=JsonUtility.FromJson<Sharon>(jsonString);
+        Debug.Log(sharonDialogs.phaseOne[0]);               
+    }
 }
